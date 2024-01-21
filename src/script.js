81aab6b4832bbfc5c5bc8a6ad6f53b937b996a45
David Liebherr <ambition.aidev@gmail.com>
2024-01-21 11:55:29 -0600

    WIP, made base advances per walk when bases loaded. need to get score working

--- src/script.js
+++ src/script.js
@@ -1,118 +1,140 @@
-let call_on_field = document.getElementById("call_on_field");
-call_on_field.innerText = "Play Ball!";
+let call_on_field = document.getElementById("call_on_field")
+call_on_field.innerText = "Play Ball!"
 
-const balls = document.getElementById("balls");
-const strikes = document.getElementById("strikes");
-const outs = document.getElementById("outs");
-const rollButton = document.querySelector("#roll_dice");
+const balls = document.getElementById("balls")
+const strikes = document.getElementById("strikes")
+const outs = document.getElementById("outs")
+const rollButton = document.querySelector("#roll_dice")
 
-const scoreCardContainer = document.querySelector("#score_card");
+const scoreCardContainer = document.querySelector("#score_card")
 //
 
-let ballCount = 0;
-let strikeCount = 0;
-let outCount = 0;
+let ballCount = 3
+let strikeCount = 0
+let outCount = 0
 
 // DOM bases
-const firstBase = document.querySelector("#first_base");
-const secondBase = document.querySelector("#second_base");
-const thirdBase = document.querySelector("#third_base");
+const firstBase = document.querySelector("#first_base")
+const secondBase = document.querySelector("#second_base")
+const thirdBase = document.querySelector("#third_base")
 
 const call = () => {
-    let diceball = [1, 2, 3, 4, 5, 6];
-    let dice_result = Math.floor(Math.random() * diceball.length + 1);
-    const even_true = dice_result % 2 === 0;
-    const diceRolled = document.querySelector("#diceRolled");
-    diceRolled.innerText = dice_result;
+    let diceball = [1, 2, 3, 4, 5, 6]
+    let dice_result = Math.floor(Math.random() * diceball.length + 1)
+    const even_true = dice_result % 2 === 0
+    const diceRolled = document.querySelector("#diceRolled")
+    diceRolled.innerText = dice_result
 
     if (even_true === true) {
-        call_on_field.innerHTML = "<i>Strike</i>";
+        call_on_field.innerHTML = "<i>Strike</i>"
         //   call_on_field.style = "color: red; font-weight: bold;"
-        strikes.innerText = `Strikes: ${strikeCount + 1}`;
-        strikeCount++;
-        out();
+        strikes.innerText = `Strikes: ${strikeCount + 1}`
+        strikeCount++
+        out()
         setInterval(() => {
-            strikes.innerText = `Strikes: ${strikeCount}`;
-        }, "4000");
-        strikeOut();
-    } else if (even_true === false) {
-        call_on_field.innerText = "Ball";
-        balls.innerText = `Balls: ${ballCount + 1}`;
-        ballCount++;
+            strikes.innerText = `Strikes: ${strikeCount}`
+         }, "4000")
+         strikeOut()
+      } else if (even_true === false) {
+         call_on_field.innerText = "Ball"
+        balls.innerText = `Balls: ${ballCount + 1}`
+        ballCount++
         setInterval(() => {
-            balls.innerText = `Balls: ${ballCount}`;
-        }, "4000");
-        walk();
+           balls.innerText = `Balls: ${ballCount}`
+         }, "4000")
+         walk()
     }
-};
+}
 
+let homeRuns = 0
+let awayRuns = 0
+let onFirst = false
+let onSecond = false
+let onThird = false
 const walk = () => {
-    if (ballCount === 4) {
-        balls.innerText = "Walk";
-        counterReset();
-        strikes.innerText = `Strikes: ${strikeCount}`;
-    }
-};
+
+    if (ballCount === 4 && !onFirst) {
+        balls.innerText = "Walk"
+        counterReset()
+        strikes.innerText = `Strikes: ${strikeCount}`
+        firstBase.style.backgroundColor = 'red'
+        onFirst = true
+      } else if(ballCount === 4 && onFirst && !onSecond) {
+         counterReset()
+         onSecond = true
+         secondBase.style.backgroundColor = 'red'
+      } else if (ballCount === 4 && onFirst && onSecond && !onThird) {
+         counterReset()
+         onThird = true
+         thirdBase.style.backgroundColor = 'red'
+      } else if (ballCount === 4 && onFirst && onSecond && onThird) {
+         counterReset()
+        console.log(awayRuns += 1)
+      }
+      
+   }
 
 const strikeOut = () => {
     if (strikeCount === 3) {
-        strikes.innerText = "Strike out!";
-        counterReset();
-        balls.innerText = `Balls: ${ballCount}`;
+        strikes.innerText = "Strike out!"
+        counterReset()
+        balls.innerText = `Balls: ${ballCount}`
     }
-};
+}
 
 const counterReset = () => {
-    strikeCount = 0;
-    ballCount = 0;
-};
+    strikeCount = 0
+    ballCount = 3
+}
 
 const out = () => {
     if (strikeCount === 3) {
-        outCount++;
-        outs.innerText = `Outs: ${outCount}`;
-        call_on_field.innerText = "OUT!";
+        outCount++
+        outs.innerText = `Outs: ${outCount}`
+        call_on_field.innerText = "OUT!"
     }
 
     if (outCount === 3) {
-        firstBase.style = "background-color: white;";
-        secondBase.style = "background-color: white;";
-        thirdBase.style = "background-color: white;";
-        outCount = 0;
-        outs.innerText = `Outs: ${outCount}`;
-        call_on_field.innerText = "Switching sides!";
+        firstBase.style = "background-color: white;"
+        secondBase.style = "background-color: white;"
+        thirdBase.style = "background-color: white;"
+        outCount = 0
+        outs.innerText = `Outs: ${outCount}`
+        call_on_field.innerText = "Switching sides!"
         setInterval(() => {
-            call_on_field.innerText = "Play Ball!";
-        }, "4000");
+            call_on_field.innerText = "Play Ball!"
+        }, "4000")
     }
-};
+}
 
 // rolling the dice button
 
 rollButton.addEventListener("click", (e) => {
-    e.preventDefault;
-    call();
-});
+    e.preventDefault
+    call()
+})
 
 // appended scoreboard
 
 function scoreBoard(awayRuns, homeRuns) {
-    const scoreCard = document.createElement("div");
-    scoreCard.className = "scoreCardCss";
-    scoreCard.textContent = "Score board: ";
+   const scoreCard = document.createElement("div")
+    scoreCard.className = "scoreCardCss"
+    scoreCard.textContent = "Score board: "
+    
+    scoreCardContainer.appendChild(scoreCard)
+    
+    const awayScore = document.createElement("div")
+    awayScore.textContent = `Away: ${awayRuns}`
+    scoreCard.appendChild(awayScore)
+    
+    const homeScore = document.createElement("div")
+    homeScore.textContent = `Home: ${homeRuns}`
+    scoreCard.appendChild(homeScore)
+   }
 
-    scoreCardContainer.appendChild(scoreCard);
+scoreBoard(awayRuns, homeRuns)
+console.log(awayRuns)
 
-    const awayScore = document.createElement("div");
-    awayScore.textContent = `Away: ${awayRuns}`;
-    scoreCard.appendChild(awayScore);
-
-    const homeScore = document.createElement("div");
-    homeScore.textContent = `Home: ${homeRuns}`;
-    scoreCard.appendChild(homeScore);
-}
-
-scoreBoard(0, 0);
 
 // ----- EXPERIMENTIAL BALL MOVEMENT CODE ----- //
 
@@ -151,6 +173,5 @@ scoreBoard(0, 0);
 // screenBody.addEventListener("click", (e) => {
 //     console.log(e.clientX, e.clientY);
 // });
-
 
 // ----- END OF BALL MOVEMENT CODE SNIPPENT ----- //
